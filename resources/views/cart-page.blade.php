8d782682dda5a95062b23454c7f36b697c18b8b9
nudlez <patrick.pinay@hotmail.com>
2022-07-11 17:46:07 +0800

    Changes to cart page

--- resources/views/cart-page.blade.php
+++ resources/views/cart-page.blade.php
@@ -5,7 +5,7 @@
         <div class="row mt-5">
             <div class="col-lg-8 col-md-8 col-sm-12 col-12 mx-auto">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 pb-3 border-bottom">
                         <span class="fs-3 fw-bold">My Cart</span>
                     </div>
                 </div>
@@ -15,9 +15,14 @@
                             <div>
                                 <small>{{ $item->name }}</small>
                             </div>
-                            <div class="d-flex flex-columns py-2 border-bottom">
-                                <div style="width:100px">
+                        </div>
+                        <div class="col-12 border-bottom pb-3">
+                            <div class="row">
+                                <div class="col-2">
                                     <img src="{{ asset('storage/images/'.$item->attributes[0]['image']) }}" alt="" class="img-fluid">
+                                </div>
+                                <div class="col-10 text-end my-auto">
+                                    &#8369;{{ number_format($item->price) }}
                                 </div>
                             </div>
                         </div>
